6ea3ccf04d3b0059eb6a03f05fd7a38bd502b25f
DaiziDev <shadowapex308@gmail.com>
2026-09-22 20:47:40 +0100

    Delete frontend-angular directory

--- frontend-angular/src/app/features/clients/client.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ClientList } from './client-list/client-list';
-import { ClientForm } from './client-form/client-form';
-
-export const CLIENT_ROUTES: Routes = [
-  { path: '', component: ClientList },
-  { path: 'nouveau', component: ClientForm },
-  { path: ':id/editer', component: ClientForm },
-];
--- frontend-angular/src/app/features/ventes/vente.routes.ts
+++
@@ -1,4 +0,0 @@
-import { Routes } from '@angular/router';
-import { PointDeVente } from './point-de-vente/point-de-vente';
-
-export const VENTE_ROUTES: Routes = [{ path: '', component: PointDeVente }];
